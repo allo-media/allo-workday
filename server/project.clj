7e6f5ecd2c12e3dab8b2ee9369c56465ae3dae63
Manon <majeasi34@gmail.com>
2018-05-09 11:33:14 +0200

    Initial import of server code

---
+++ server/project.clj
@@ -0,0 +1,14 @@
+(defproject hello-world "0.1.0-SNAPSHOT"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [compojure "1.5.1"]
+                 [ring/ring-defaults "0.2.1"]
+                 [org.clojure/java.jdbc "0.7.5"]
+                 [org.xerial/sqlite-jdbc "3.7.2"]
+                 [org.clojure/data.json "0.2.6" ]]
+  :plugins [[lein-ring "0.9.7"]
+            [lein-cljfmt "0.5.7"]]
+  :ring {:handler hello-world.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring/ring-mock "0.3.0"]]}})
---
+++ server/src/hello_world/handler.clj
@@ -0,0 +1,29 @@
+(ns hello-world.handler
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [clojure.java.jdbc :as jdbc]
+            [clojure.data.json :as json]))
+
+(def db
+  {:classname   "org.sqlite.JDBC"
+   :subprotocol "sqlite"
+   :subname     "db/database.db"})
+
+(defn get_projects
+  []
+  (let [results (jdbc/query db ["SELECT * FROM Projects"])]
+    (prn results)
+
+    (cond (empty? results)
+        ("Empty Db")
+        :else
+        (lazy-seq results))))
+
+(defroutes app-routes
+  (GET "/" [] "Hello World")
+  (GET "/projects" [] (json/write-str (get_projects)))
+  (route/not-found "Not Found"))
+
+(def app
+  (wrap-defaults app-routes site-defaults))
