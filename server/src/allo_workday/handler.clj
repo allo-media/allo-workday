c6476c8e17cb079050558a9d6698876546058b4a
Manon <majeasi34@gmail.com>
2018-05-14 15:00:08 +0200

    Change on BDD and taking care of R&D projects

--- server/src/allo_workday/handler.clj
+++ server/src/allo_workday/handler.clj
@@ -18,6 +18,22 @@
           :else
           (lazy-seq results))))
 
+(defn insert_projects
+  [list_to_insert id_date]
+  (cond (empty? list_to_insert)
+        (println "End of Recursion")
+        :else
+        (let [id_projects (or (get (first list_to_insert) :id)
+                              "ID_failed")
+              hours (or (get (first list_to_insert) :hours)
+                        "HOURS_failed")]
+          ;(println id_projects " " hours)
+          (jdbc/insert! db :Projects_On_Days
+                        {:Heures hours :ID_Users 1 :ID_Date id_date :Id_Projects id_projects})
+          (insert_projects (rest list_to_insert) id_date))))
+;)
+
+
 (defn post_days
   [req]
 
@@ -32,10 +48,14 @@
                       "AFTERNOON_failed")
         comments (or (get-in req [:params :comment])
                      (get-in req [:body :comment])
-                     "COMMENTS_failed")]
+                     "COMMENTS_failed")
+        list_projects (or (get-in req [:params :projects])
+                          (get-in req [:body :projects])
+                          "PROJECTS_failed")]
 
-    (jdbc/insert! db :Days
-                  {:Date date :Morning morning :Afternoon afternoon :Comments comments})))
+    (let [key_date (first (vals (first (jdbc/insert! db :Days
+                                                     {:ID_Users 1 :Date date :Morning morning :Afternoon afternoon :Comments comments}))))]
+      (insert_projects list_projects key_date))))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
