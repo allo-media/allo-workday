1d93cb484876ba931525a0ae58b69a487ce3a1b0
Fabrice Bentz <fabrice@weeple.fr>
2018-07-26 16:45:54 +0200

    Remove useless file

--- client/src/Request/Github.elm
+++
@@ -1,9 +0,0 @@
-module Request.Github exposing (getReadme)
-
-import Data.Session exposing (Session)
-import Http exposing (Request, getString)
-
-
-getReadme : Session -> Request String
-getReadme _ =
-    getString "https://raw.githubusercontent.com/allo-media/elm-kitchen/master/README.md"
