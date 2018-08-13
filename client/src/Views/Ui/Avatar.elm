b701a271dd941d1d5b50031614f0702b14c47bd2
Fabrice Bentz <fabrice@weeple.fr>
2018-08-13 16:19:25 +0200

    Add avatar ui module.

---
+++ client/src/Views/Ui/Avatar.elm
@@ -0,0 +1,21 @@
+module Views.Ui.Avatar exposing (medium)
+
+import Css exposing (..)
+import Html.Styled exposing (..)
+import Views.Theme exposing (Element, identify, theme)
+
+
+identify_ : String -> Style
+identify_ string =
+    "Value.Ui.Avatar." ++ string |> identify
+
+
+medium : Element msg
+medium =
+    styled img
+        [ identify_ "medium"
+        , borderRadius (pct 50)
+        , border3 (px 4) solid (rgb 255 255 255)
+        , width (px 56)
+        , height (px 56)
+        ]
