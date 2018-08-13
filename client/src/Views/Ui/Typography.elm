521f7d9d0a8f4001596aab9ead6823832d9068b0
Fabrice Bentz <fabrice@weeple.fr>
2018-08-13 16:54:16 +0200

    Add new function et change string for identify_

--- client/src/Views/Ui/Typography.elm
+++ client/src/Views/Ui/Typography.elm
@@ -1,19 +1,20 @@
 module Views.Ui.Typography exposing (..)
 
 import Css exposing (..)
-import Html.Styled exposing (h1, styled)
+import Html.Styled exposing (h1, p, styled)
 import Views.Theme exposing (Element, identify)
 
 
 identify_ : String -> Style
 identify_ string =
-    "Views.UI.Panel." ++ string |> identify
+    "Views.UI.Typograhy." ++ string |> identify
 
 
 heading : Element msg
 heading =
     styled h1
         [ identify_ "heading"
+        , textTransform uppercase
         , color (hex "FFF")
         ]
 
@@ -25,4 +26,12 @@ headingLogin =
         , textTransform uppercase
         , textAlign center
         , fontSize (Css.rem 4)
+        ]
+
+
+primaryHeaderText : Element msg
+primaryHeaderText =
+    styled p
+        [ fontSize (Css.rem 1.5)
+        , color (rgb 255 255 255)
         ]
