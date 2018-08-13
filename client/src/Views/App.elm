d370bbef1d5a9881876c1fbc023dff3cbf301878
Fabrice Bentz <fabrice@weeple.fr>
2018-08-13 13:00:29 +0200

    Delete App module.

--- client/src/Views/App.elm
+++
@@ -1,9 +0,0 @@
-module Views.App exposing (view)
-
-import Html.Styled exposing (Html, div)
-import Views.Theme exposing (defaultCss)
-
-
-view : Html msg -> Html msg
-view content =
-    div [] [ defaultCss, content ]
