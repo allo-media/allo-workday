e8016621ad224c6f047d47e91b89d83386b822b0
Fabrice Bentz <fabrice@weeple.fr>
2018-08-13 16:53:54 +0200

    Update ui logo.

--- client/src/Views/Ui/Logo.elm
+++ client/src/Views/Ui/Logo.elm
@@ -1,8 +1,14 @@
-module Views.Ui.Logo exposing (medium)
+module Views.Ui.Logo exposing (medium, small)
 
 import Css exposing (..)
 import Html.Styled exposing (..)
+import Html.Styled.Attributes exposing (css, src)
 import Views.Theme exposing (Element, identify)
+
+
+logoPath : String
+logoPath =
+    "assets/logoWorkday.svg"
 
 
 identify_ : String -> Style
@@ -12,15 +18,32 @@ identify_ string =
 
 default : Element msg
 default =
-    styled img
-        [ identify_ "default"
-        ]
+    styled img [ identify_ "default" ]
 
 
-medium : Element msg
+medium : Html msg
 medium =
-    styled default
-        [ identify_ "medium"
-        , width (px 158)
-        , height (px 158)
+    default
+        [ css
+            [ identify_ "medium"
+            , width (px 158)
+            , height (px 158)
+            , property "justify-self" "center"
+            ]
+        , src logoPath
         ]
+        []
+
+
+small : Html msg
+small =
+    default
+        [ css
+            [ identify_ "small"
+            , width (px 60)
+            , height (px 60)
+            , property "justify-self" "center"
+            ]
+        , src logoPath
+        ]
+        []
